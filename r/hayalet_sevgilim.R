e8b7b69540e948aaec906e6a1a71559a369a26a4
kilimcinin kör oğlu <229108989+KilimcininKorOglu@users.noreply.github.com>
2025-10-01 16:34:36 +0300

    Create hayalet_sevgilim.R

---
+++ r/hayalet_sevgilim.R
@@ -0,0 +1,79 @@
+# Hayalet Sevgilim - Song Lyrics
+# Author: İrem Aydın
+
+lyrics <- c(
+  "Ceza mı bu",
+  "Çektiğim çile mi",
+  "Yıllardır tuttuğum nöbet bitmeyecek mi?",
+  "Bir küçük kar tanesi gibiyim",
+  "Avucunda eriyen dön bebeğim",
+  "",
+  "Gözyaşlarını görürsem",
+  "Erir kanatlarım",
+  "Uçamam rüyalarında yanına",
+  "Sonsuzluk senle başladı",
+  "O küçük dünyamda",
+  "Unutma gittiğinde yarım kaldım",
+  "",
+  "Çöllerdeyim yanıyorum",
+  "Kutuptayım üşüyorum",
+  "Ceza benim çekiyorum ne olur dön",
+  "Uzanıyorum tutamıyorum",
+  "Özlüyorum ağlıyorum",
+  "Yasak mısın anlamıyorum ne olur dön",
+  "",
+  "Sevmesen de beni özledim sesini",
+  "Git desem de yine gitmesen",
+  "Yıllardır çektiğim bu hasret mi çile mi?",
+  "Haram mısın bana bi' bilsem",
+  "",
+  "Sevmesen de beni özledim sesini",
+  "Git desem de yine gitmesen",
+  "Yıllardır çektiğim bu hasret mi çile mi?",
+  "Haram mısın bana bi' bilsem",
+  "",
+  "Bebeğim benim, hayalet sevgilim",
+  "Bebeğim benim, hayalet sevgilim",
+  "",
+  "Hayalet sevgilim",
+  "",
+  "Çöllerdeyim yanıyorum",
+  "Kutuptayım üşüyorum",
+  "Ceza benim çekiyorum ne olur dön",
+  "Uzanıyorum tutamıyorum",
+  "Özlüyorum ağlıyorum",
+  "Yasak mısın anlamıyorum ne olur dön",
+  "",
+  "Sevmesen de beni özledim sesini",
+  "Git desem de yine gitmesen",
+  "Yıllardır çektiğim bu hasret mi çile mi?",
+  "Haram mısın bana bi' bilsem",
+  "",
+  "Sevmesen de beni özledim sesini",
+  "Git desem de yine gitmesen",
+  "Yıllardır çektiğim bu hasret mi çile mi?",
+  "Haram mısın bana bi' bilsem",
+  "",
+  "Bebeğim benim hayalet sevgilim",
+  "Bebeğim benim hayalet sevgilim",
+  "",
+  "Hayalet sevgilim",
+  "Hayalet sevgilim"
+)
+
+# Print all lyrics
+print_lyrics <- function() {
+  for (line in lyrics) {
+    cat(line, "\n")
+  }
+}
+
+# Function to get lyrics as vector
+get_lyrics <- function() {
+  return(lyrics)
+}
+
+# Main execution
+if (!interactive()) {
+  print_lyrics()
+}
